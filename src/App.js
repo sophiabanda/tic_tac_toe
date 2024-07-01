543acd6b7ff15196c2f8e6bdb81db3621ecc68e1
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2024-07-01 12:38:53 -0400

    Add webpack rules to include pre-existing css file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  return <div>This was made from scratch! Hello, world!</div>;
+  return <div className="test">This was made from scratch! Hello, world!</div>;
 };
 
 export default App;
