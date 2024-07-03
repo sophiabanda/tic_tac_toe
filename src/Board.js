50758c6724e488720eef775139a90862d624638a
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2024-07-02 20:55:14 -0400

    update to createRoot from ReactDOM.render for React

--- src/Board.js
+++ src/Board.js
@@ -1,32 +1,33 @@
 import React, { useState } from 'react';
 
-function Square() {
-  function handleClick() {
-    alert('Clicked!');
-  }
-
+function Square({ value, onSquareClick }) {
   return (
-    <button className="cell" onClick={handleClick}>
-      X
+    <button className="cell" onClick={onSquareClick}>
+      {value}
     </button>
   );
 }
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick() {
+    setSquares('X');
+  }
+
   return (
     <>
       <div className="container">
         <div className="board">
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
+          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
         </div>
       </div>
     </>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App.js';
 import './App.css';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const container = document.getElementById('app');
+const root = createRoot(container);
+
+root.render(<App tab="home" />);
